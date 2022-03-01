69b119c86b8615bd80c711ef59d05d33f72b8f2b
Estudiante <ignmmartin@gmail.com>
2022-03-01 10:41:57 +0000

    Modificado el application

--- src/main/java/com/example/demo/ProyectoSpringBootMvcApplication.java
+++ src/main/java/com/example/demo/ProyectoSpringBootMvcApplication.java
@@ -2,9 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProyectoSpringBootMvcApplication {
+public class ProyectoSpringBootMvcApplication extends SpringBootServletInitializer {
+	
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+				return builder.sources(ProyectoSpringBootMvcApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoSpringBootMvcApplication.class, args);
